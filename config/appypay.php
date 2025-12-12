5078b8d6029bb7c9088184ca199512b468b0ec04
Gomes19 <gomesfranciscomateus34@gmail.com>
2025-12-12 16:59:55 +0100

    updating config

--- config/appypay.php
+++ config/appypay.php
@@ -2,10 +2,10 @@
 
 return [
     'base_url' => env('APPYPAY_BASE_URL', 'https://gwy-api.appypay.co.ao/v2.0'),
-    'token_url' => env('APPYPAY_TOKEN_URL', 'https://auth.appypay.co.ao/connect/token'),
+    'token_url' => env('APPYPAY_TOKEN_URL', 'https://login.microsoftonline.com/auth.appypay.co.ao/oauth2/token'),
     'client_id' => env('APPYPAY_CLIENT_ID'),
     'client_secret' => env('APPYPAY_CLIENT_SECRET'),
-    'resource' => env('APPYPAY_RESOURCE', 'https://gwy-api.appypay.co.ao'),
+    'resource' => env('APPYPAY_RESOURCE', 'bee57785-7a19-4f1c-9c8d-aa03f2f0e333'),
     'timeout' => env('APPYPAY_TIMEOUT', 120),
     'connect_timeout' => env('APPYPAY_CONNECT_TIMEOUT', 15),
     'accept_language' => env('APPYPAY_ACCEPT_LANGUAGE', 'pt-AO'),
